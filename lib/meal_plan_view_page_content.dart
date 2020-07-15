10c74e9edf11a175acddb938951aca49d117ecad
etka4463 <etka4463@colorado.edu>
2020-07-15 10:34:01 -0600

    Added meal cards to meal plan view page

--- lib/meal_plan_view_page_content.dart
+++ lib/meal_plan_view_page_content.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'top_bar.dart';
+import 'meal_card.dart';
 
 class MealPlanViewPage extends StatefulWidget {
   final String meal_plan_name;
@@ -20,7 +21,30 @@ class _MealPlanViewPageState extends State<MealPlanViewPage> {
             subheader: "View Plan",
             subheader_content: null,
             back_button: true,
-          )
+          ),
+          Expanded(
+            flex: 1,
+            child: Scrollbar(
+              child: GridView.count(
+                crossAxisCount: 2,
+                padding: EdgeInsets.all(8),
+                children: <Widget>[
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                  MealCard(),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
