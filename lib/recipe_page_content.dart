d6365581f7f29f13d2d22e19aa7e100047d79834
etka4463 <etka4463@colorado.edu>
2020-07-21 12:04:59 -0600

    Added Animated Switcher for ingredients and instructions on recipe page

--- lib/recipe_page_content.dart
+++ lib/recipe_page_content.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'top_bar.dart';
 
@@ -11,6 +12,13 @@ class RecipePage extends StatefulWidget {
 }
 
 class _RecipePageState extends State<RecipePage> {
+  List<Widget> _myAnimatedWidgets = [
+    IngredientsWidget(),
+    InstructionsWidget(),
+  ];
+
+  int selectorValue = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +30,64 @@ class _RecipePageState extends State<RecipePage> {
             subheader_content: null,
             back_button: true,
           ),
+          SizedBox(height: 30),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: CupertinoSegmentedControl(
+                  onValueChanged: (int value) {
+                    setState(() {
+                      selectorValue = value;
+                    });
+                  },
+                  children: <int, Widget>{
+                    0: Text("Ingredients"),
+                    1: Text("Instructions"),
+                  },
+                  groupValue: selectorValue,
+                  selectedColor: green_accent_color,
+                  borderColor: green_accent_color,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: AnimatedSwitcher(
+              child: _myAnimatedWidgets[selectorValue],
+              duration: Duration(milliseconds: 500),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return ScaleTransition(
+                  scale: animation,
+                  child: child,
+                );
+              },
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class IngredientsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class InstructionsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+      child: Container(
+        color: Colors.blue,
       ),
     );
   }
